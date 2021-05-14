ce7a1ccb90ae209e3d7cbb578a706f91426fc0cf
00anupam00 <rakshit.anupam@hotmail.com>
2021-05-14 07:10:06 +0300

    appendices

--- spark-submit.sh
+++ spark-submit.sh
@@ -1,2 +1,10 @@
-#spark-submit --deploy-mode cluster --master k8s://https://127.0.0.1:16443 --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-sa --name spark-pi --class pi.py --conf spark.executor.instances=2  --conf spark.kubernetes.executor.container.image=00anupam00/spark-py /home/ubuntu/spark/spark-3.1.1-bin-hadoop2.7/examples/src/main/python/pi
-spark-submit   --master spark://172.17.65.44:7077   outlier-detection/main.py -erandom_forest
+spark-submit --deploy-mode cluster --master k8s://https://127.0.0.1:16443 --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-sa --name spark-pi --class pi.py --conf spark.executor.instances=2  --conf spark.kubernetes.executor.container.image=00anupam00/spark-py /home/ubuntu/spark/spark-3.1.1-bin-hadoop2.7/examples/src/main/python/pi
+
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077 --conf spark.eventLog.enabled=false ws/outlier-detection/main.py --estimator decision_tree
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077 --num-executors 5 --executor-cores 3 --archives pyspark_venv.tar.gz  ws/outlier-detection/main.py --estimator decision_tree
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077  ws/outlier-detection/main.py --estimator random_forest
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077 --num-executors 5 --executor-cores 3 --archives pyspark_venv.tar.gz  ws/outlier-detection/main.py --estimator random_forest
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077 ws/outlier-detection/main.py --estimator perceptron
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077 --conf spark.eventLog.enabled=false --archives pyspark_venv.tar.gz  ws/outlier-detection/main.py --estimator perceptron
+
+spark-submit  --master spark://rakshit-thesis.cloud.ut.ee:7077 --num-executors 5 --executor-cores 3  --executor-memory 10GB --archives pyspark_venv.tar.gz  ws/outlier-detection/main.py --estimator perceptron
--- src/Tuning.py
+++ src/Tuning.py
@@ -4,20 +4,14 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit, CrossValidator
 from pyspark.sql import dataframe
 
-from src.utils.Estimators import get_estimator
+from src.utils.Estimators import get_estimator, get_perceptron_estimator, get_estimator_for_multiclass
 from src.multiclass.DataPreProcessor import load_dataset_with_categories
 
 
-def evaluate_with_train_validation_split(df: dataframe.DataFrame, estimator, evaluator):
+def evaluate_with_train_validation_split(df: dataframe.DataFrame, estimator, pipeline, evaluator):
     train, test = df.randomSplit([0.9, 0.1], seed=12345)
 
-    assembler = VectorAssembler(
-        inputCols=[x for x in df.columns if x != "label"],
-        outputCol="features"
-    )
-
     algorithm = get_estimator(estimator)
-    pipeline = Pipeline(stages=[assembler, algorithm])
     paramGrid = ParamGridBuilder() \
         .addGrid(algorithm.numTrees, [5, 10, 20]) \
         .addGrid(algorithm.maxDepth, [5, 10]) \
@@ -35,17 +29,20 @@ def evaluate_with_train_validation_split(df: dataframe.DataFrame, estimator, eva
     print("from train validation split hyparam tuner:")
     prediction.show(5)
 
-def evaluate_with_cross_validation(df: dataframe.DataFrame, pipeline, estimator: Estimator):
+
+def evaluate_with_cross_validation(df: dataframe.DataFrame, pipeline, estimator, evaluator):
     train, test = df.randomSplit([0.9, 0.1], seed=12345)
 
-    algo = get_estimator(estimator)
+    algorithm = get_estimator(estimator)
     paramGrid = ParamGridBuilder() \
-        .addGrid(algo.regParam, [0.1, 0.01]) \
+        .addGrid(algorithm.numTrees, [5, 10, 20]) \
+        .addGrid(algorithm.maxDepth, [5, 10]) \
+        .addGrid(algorithm.cacheNodeIds, [True, False]) \
         .build()
 
     crossval = CrossValidator(estimator=pipeline,
                               estimatorParamMaps=paramGrid,
-                              evaluator=BinaryClassificationEvaluator(),
+                              evaluator=evaluator,
                               numFolds=2)
 
     model = crossval.fit(train)
@@ -54,7 +51,24 @@ def evaluate_with_cross_validation(df: dataframe.DataFrame, pipeline, estimator:
     prediction.show()
 
 
-if __name__ == '__main__':
-    df = load_dataset_with_categories()
-    df.show(10)
-    evaluate_with_cross_validation(df, "random_forest", MulticlassClassificationEvaluator())
+def get_pipeline(df, estimator):
+    assembler = VectorAssembler(
+        inputCols=[x for x in df.columns if x != "label"],
+        outputCol="features",
+        handleInvalid="skip"
+    )
+    algo = ()
+    if estimator.lower() == "perceptron":
+        features_col = len(assembler.getInputCols())
+        print("Number of features column: ", str(features_col))
+        algo = get_perceptron_estimator(features_col)
+    else:
+        algo = get_estimator_for_multiclass(estimator)
+    return Pipeline().setStages([assembler, algo])
+
+#
+# if __name__ == '__main__':
+#     df = load_dataset_with_categories()
+#     estimator = "random_forest"
+#     evaluate_with_cross_validation(df, get_pipeline(estimator), estimator, MulticlassClassificationEvaluator())
+#     evaluate_with_train_validation_split(df, estimator, get_pipeline(estimator), MulticlassClassificationEvaluator())
