ed3ab2c030a1d98ec5e9b6cd2a1998a93076d04a
00anupam00 <rakshit.anupam@hotmail.com>
2021-07-11 11:18:11 +0300

    PCA impl

--- src/multiclass/MulticlassPipeline.py
+++ src/multiclass/MulticlassPipeline.py
@@ -1,6 +1,6 @@
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import VectorAssembler, PCA
 from pyspark.sql import dataframe
 
 from src.Tuning import evaluate_with_cross_validation, evaluate_with_train_validation_split, get_pipeline
@@ -10,9 +10,12 @@ from src.utils.Estimators import get_estimator_for_multiclass, get_perceptron_es
 def process_multiclass_pipeline(df: dataframe.DataFrame, estimator):
     assembler = VectorAssembler(
         inputCols=[x for x in df.columns if x != "label"],
-        outputCol="features",
+        outputCol="features_v",
         handleInvalid="skip"
     )
+
+    # PCA
+    pca = PCA(k=23, inputCol="features_v", outputCol="features")
 
     algo = ()
     if estimator.lower() == "perceptron":
@@ -25,9 +28,7 @@ def process_multiclass_pipeline(df: dataframe.DataFrame, estimator):
     # Split the data into training and test sets (30% held out for testing)
     (trainingData, testData) = df.randomSplit([0.7, 0.3])
 
-    # feature_selector =  get_selector("features", "label")
-
-    pipeline = Pipeline().setStages([assembler, algo])
+    pipeline = Pipeline().setStages([assembler, pca, algo])
 
     # Tune multiclass
     tdf_cross, tdf_train = tune_multiclass(df, estimator)
@@ -35,9 +36,7 @@ def process_multiclass_pipeline(df: dataframe.DataFrame, estimator):
     pipeline_model = pipeline.fit(trainingData)
     transformed_df = pipeline_model.transform(testData)
 
-    # print("Schema after transformation:")
     print("Transformed df:")
-    transformed_df.printSchema()
     transformed_df.select("id", "features", "prediction", "rawPrediction", "probability", "label").show(5)
 
     return transformed_df, tdf_cross, tdf_train
