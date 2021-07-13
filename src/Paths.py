69bd7e933ed6c619891a62a7a5447c70fa0a1fbb
Ubuntu <ubuntu@rakshit-thesisv2.cloud.ut.ee>
2021-07-13 06:01:29 +0000

    changes in ubuntu dataset paths

--- src/Paths.py
+++ src/Paths.py
@@ -1,10 +1,26 @@
+#
+#
+#<<<<<<< Updated upstream
+#ssl_reneg_dataset = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/ssl/dataset_min.csv"
+#ssl_reneg_labels = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/ssl/labels_min.csv"
+#
+#arp_spoof_dataset = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/arp/dataset_min.csv"
+#arp_spoof_labels = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/arp/labels_min.csv"
+#
+#syn_dos_dataset = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/syn/dataset_min.csv"
+#syn_dos_labels = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/syn/labels_min.csv"
+#=======
+ssl_reneg_dataset = "/home/ubuntu/outlier-detection/input/ssl/dataset.csv"
+ssl_reneg_labels = "/home/ubuntu/outlier-detection/input/ssl/labels.csv"
+
+arp_spoof_dataset = "/home/ubuntu/outlier-detection/input/arp/dataset.csv"
+arp_spoof_labels = "/home/ubuntu/outlier-detection/input/arp/labels.csv"
+
+syn_dos_dataset = "/home/ubuntu/outlier-detection/input/syn/dataset.csv"
+syn_dos_labels = "/home/ubuntu/outlier-detection/input/syn/labels.csv"
+
+mirai_dataset = "/home/ubuntu/outlier-detection/input/mirai/dataset.csv"
+mirai_labels = "/home/ubuntu/outlier-detection/input/mirai/labels.csv"
 
 
-ssl_reneg_dataset = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/ssl/dataset_min.csv"
-ssl_reneg_labels = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/ssl/labels_min.csv"
-
-arp_spoof_dataset = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/arp/dataset_min.csv"
-arp_spoof_labels = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/arp/labels_min.csv"
-
-syn_dos_dataset = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/syn/dataset_min.csv"
-syn_dos_labels = "/Users/anupamrakshit/Google Drive/TalTech/Thesis/outlier-detection/input/syn/labels_min.csv"
+#>>>>>>> Stashed changes
