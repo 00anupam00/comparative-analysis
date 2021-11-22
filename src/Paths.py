b55e53efd29933bbd4c720be834c7fc1c9b60812
Ubuntu <ubuntu@rakshit-thesisv2.cloud.ut.ee>
2021-11-22 20:08:05 +0000

    path change for linux

--- src/Paths.py
+++ src/Paths.py
@@ -3,12 +3,24 @@ import os
 
 basePath = os.path.abspath(os.getcwd())
 
-ssl_reneg_dataset = basePath + "/input/ssl/dataset.csv"
-ssl_reneg_labels = basePath + "/input/ssl/labels.csv"
+#
+#ssl_reneg_dataset = basePath + "/input/ssl/dataset.csv"
+#ssl_reneg_labels = basePath + "/input/ssl/labels.csv"
+#
+#arp_spoof_dataset = basePath + "/input/arp/dataset.csv"
+#arp_spoof_labels = basePath + "/input/arp/labels.csv"
+#
+#syn_dos_dataset = basePath + "/input/syn/dataset.csv"
+#syn_dos_labels = basePath + "/input/syn/labels.csv"
+#
 
-arp_spoof_dataset = basePath + "/input/arp/dataset.csv"
-arp_spoof_labels = basePath + "/input/arp/labels.csv"
+ssl_reneg_dataset = basePath + "/comparative-analysis/input/ssl/dataset.csv"
+ssl_reneg_labels = basePath + "/comparative-analysis/input/ssl/labels.csv"
 
-syn_dos_dataset = basePath + "/input/syn/dataset.csv"
-syn_dos_labels = basePath + "/input/syn/labels.csv"
+arp_spoof_dataset = basePath + "/comparative-analysis/input/arp/dataset.csv"
+arp_spoof_labels = basePath + "/comparative-analysis/input/arp/labels.csv"
+
+syn_dos_dataset = basePath + "/comparative-analysis/input/syn/dataset.csv"
+syn_dos_labels = basePath + "/comparative-analysis/input/syn/labels.csv"
+
 
